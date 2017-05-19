b89111d4bf56a1a6f821f1e5614b978bd8d4df5b
pablooCM <jopablo13@gmail.com>
2017-05-19 14:34:27 -0500

    Registro de clientes listo

--- ModuloIntegracion/RegistrarCliente.Designer.cs
+++ ModuloIntegracion/RegistrarCliente.Designer.cs
@@ -28,19 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxCedulaCliente = new System.Windows.Forms.TextBox();
+            this.textBoxNombreCliente = new System.Windows.Forms.TextBox();
+            this.textBoxPrimerApellido = new System.Windows.Forms.TextBox();
+            this.textBoxSegundoApellido = new System.Windows.Forms.TextBox();
+            this.textBoxCiudad = new System.Windows.Forms.TextBox();
+            this.textBoxPais = new System.Windows.Forms.TextBox();
+            this.textBoxCorreoElectronico = new System.Windows.Forms.TextBox();
+            this.buttonRegistrarCliente = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cédula cliente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Primer Apellido: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Segundo Apellido: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ciudad:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "País:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Correo Electrónico:";
+            // 
+            // textBoxCedulaCliente
+            // 
+            this.textBoxCedulaCliente.Location = new System.Drawing.Point(123, 18);
+            this.textBoxCedulaCliente.Name = "textBoxCedulaCliente";
+            this.textBoxCedulaCliente.Size = new System.Drawing.Size(189, 20);
+            this.textBoxCedulaCliente.TabIndex = 7;
+            // 
+            // textBoxNombreCliente
+            // 
+            this.textBoxNombreCliente.Location = new System.Drawing.Point(123, 48);
+            this.textBoxNombreCliente.Name = "textBoxNombreCliente";
+            this.textBoxNombreCliente.Size = new System.Drawing.Size(189, 20);
+            this.textBoxNombreCliente.TabIndex = 8;
+            // 
+            // textBoxPrimerApellido
+            // 
+            this.textBoxPrimerApellido.Location = new System.Drawing.Point(123, 82);
+            this.textBoxPrimerApellido.Name = "textBoxPrimerApellido";
+            this.textBoxPrimerApellido.Size = new System.Drawing.Size(189, 20);
+            this.textBoxPrimerApellido.TabIndex = 9;
+            // 
+            // textBoxSegundoApellido
+            // 
+            this.textBoxSegundoApellido.Location = new System.Drawing.Point(123, 114);
+            this.textBoxSegundoApellido.Name = "textBoxSegundoApellido";
+            this.textBoxSegundoApellido.Size = new System.Drawing.Size(189, 20);
+            this.textBoxSegundoApellido.TabIndex = 10;
+            // 
+            // textBoxCiudad
+            // 
+            this.textBoxCiudad.Location = new System.Drawing.Point(123, 149);
+            this.textBoxCiudad.Name = "textBoxCiudad";
+            this.textBoxCiudad.Size = new System.Drawing.Size(189, 20);
+            this.textBoxCiudad.TabIndex = 11;
+            // 
+            // textBoxPais
+            // 
+            this.textBoxPais.Location = new System.Drawing.Point(123, 180);
+            this.textBoxPais.Name = "textBoxPais";
+            this.textBoxPais.Size = new System.Drawing.Size(189, 20);
+            this.textBoxPais.TabIndex = 12;
+            // 
+            // textBoxCorreoElectronico
+            // 
+            this.textBoxCorreoElectronico.Location = new System.Drawing.Point(123, 208);
+            this.textBoxCorreoElectronico.Name = "textBoxCorreoElectronico";
+            this.textBoxCorreoElectronico.Size = new System.Drawing.Size(189, 20);
+            this.textBoxCorreoElectronico.TabIndex = 13;
+            // 
+            // buttonRegistrarCliente
+            // 
+            this.buttonRegistrarCliente.Location = new System.Drawing.Point(159, 274);
+            this.buttonRegistrarCliente.Name = "buttonRegistrarCliente";
+            this.buttonRegistrarCliente.Size = new System.Drawing.Size(201, 23);
+            this.buttonRegistrarCliente.TabIndex = 14;
+            this.buttonRegistrarCliente.Text = "Registrar Cliente";
+            this.buttonRegistrarCliente.UseVisualStyleBackColor = true;
+            this.buttonRegistrarCliente.Click += new System.EventHandler(this.buttonRegistrarCliente_Click);
             // 
             // RegistrarCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(413, 472);
+            this.Controls.Add(this.buttonRegistrarCliente);
+            this.Controls.Add(this.textBoxCorreoElectronico);
+            this.Controls.Add(this.textBoxPais);
+            this.Controls.Add(this.textBoxCiudad);
+            this.Controls.Add(this.textBoxSegundoApellido);
+            this.Controls.Add(this.textBoxPrimerApellido);
+            this.Controls.Add(this.textBoxNombreCliente);
+            this.Controls.Add(this.textBoxCedulaCliente);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "RegistrarCliente";
             this.Text = "RegistrarCliente";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxCedulaCliente;
+        private System.Windows.Forms.TextBox textBoxNombreCliente;
+        private System.Windows.Forms.TextBox textBoxPrimerApellido;
+        private System.Windows.Forms.TextBox textBoxSegundoApellido;
+        private System.Windows.Forms.TextBox textBoxCiudad;
+        private System.Windows.Forms.TextBox textBoxPais;
+        private System.Windows.Forms.TextBox textBoxCorreoElectronico;
+        private System.Windows.Forms.Button buttonRegistrarCliente;
     }
 }
--- ModuloIntegracion/RegistrarCliente.cs
+++ ModuloIntegracion/RegistrarCliente.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace ModuloIntegracion
 {
     public partial class RegistrarCliente : Form
@@ -15,6 +15,60 @@ namespace ModuloIntegracion
         public RegistrarCliente()
         {
             InitializeComponent();
+        }
+
+        private void buttonRegistrarCliente_Click(object sender, EventArgs e)
+        {
+            System.Data.SqlClient.SqlConnection conexion = new System.Data.SqlClient.SqlConnection();
+            conexion.ConnectionString = "Data Source=WIN-1SDP8NVLN2A\\SA;Initial Catalog=BDMercadeoFinal;Persist Security Info=True;User ID=sa;Password=claveParaAvanzadas2016!";
+            conexion.Open();
+
+            SqlCommand cmd = new SqlCommand("dbo.insertaCliente", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            var param1 = new SqlParameter("@cedula", SqlDbType.Int);
+            param1.Direction = ParameterDirection.Input;
+            param1.Value = textBoxCedulaCliente.Text;
+            cmd.Parameters.Add(param1);
+
+            var param2 = new SqlParameter("@nombre", SqlDbType.VarChar);
+            param2.Direction = ParameterDirection.Input;
+            param2.Value = textBoxNombreCliente.Text;
+            param2.Size = 50;
+            cmd.Parameters.Add(param2);
+
+            var param3 = new SqlParameter("@apellido1", SqlDbType.VarChar);
+            param3.Direction = ParameterDirection.Input;
+            param3.Value = textBoxPrimerApellido.Text;
+            param3.Size = 50;
+            cmd.Parameters.Add(param3);
+
+            var param4 = new SqlParameter("@apellido2", SqlDbType.VarChar);
+            param4.Direction = ParameterDirection.Input;
+            param4.Value = textBoxSegundoApellido.Text;            param4.Size = 50;
+            cmd.Parameters.Add(param4);
+
+            var param5 = new SqlParameter("@ciudad", SqlDbType.VarChar);
+            param5.Direction = ParameterDirection.Input;
+            param5.Value = textBoxCiudad.Text;
+            param5.Size = 50;
+            cmd.Parameters.Add(param5);
+
+            var param6 = new SqlParameter("@pais", SqlDbType.VarChar);
+            param6.Direction = ParameterDirection.Input;
+            param6.Value = textBoxPais.Text;
+            param6.Size = 50;
+            cmd.Parameters.Add(param6);
+
+            var param7 = new SqlParameter("@correoElectronico", SqlDbType.VarChar);
+            param7.Direction = ParameterDirection.Input;
+            param7.Value = textBoxCorreoElectronico.Text;
+            param7.Size = 50;
+            cmd.Parameters.Add(param7);
+
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+            MessageBox.Show("Cliente creado correctamente");
         }
     }
 }
